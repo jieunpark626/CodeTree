d0965b326d7cd50de145d64a3473aa4cf230704b
Jieun Park <140596167+jieunpark626@users.noreply.github.com>
2025-06-22 23:59:41 +0900

    Title: 1차원 윷놀이, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250622/1차원 윷놀이/yutnori-1d.py
+++ 250622/1차원 윷놀이/yutnori-1d.py
@@ -2,7 +2,7 @@ n, m, k = map(int, input().split())  # n : 턴 수, m : 판, k: 말의 수
 nums = list(map(int, input().split()))
 
 arr = []
-hscore = [0] * k # 각 말의 위치
+hscore = [1] * k # 각 말의 위치
 
 # idx = 턴 수
 answer = 0
@@ -16,7 +16,6 @@ def Choose(idx,score):
     for i in range(k):
         if(hscore[i] >= m):
             continue
-        
         arr.append(i)
         hscore[i] += nums[idx]
         if(hscore[i]>=m):
