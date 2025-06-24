be1fd32ae91de2caca66537e0d42bf3e22a45283
Jieun Park <140596167+jieunpark626@users.noreply.github.com>
2025-06-24 15:34:27 +0900

    Title: 최소 점프 횟수, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250624/최소 점프 횟수/min-num-of-jumps.py
@@ -0,0 +1,21 @@
+n = int(input()) # 노드 수
+num = list(map(int,input().split()))
+
+def dfs(node):
+    if(node == n-1): # 끝에 도달
+        return 0
+    if(num[node] == 0): # 끝에 도달하지 않았는데 멈추면
+        return -1 # 실패
+    
+    result = []    
+    for i in range(1,num[node]+1):
+        child = dfs(node+i)
+        
+        if(child == -1):
+            return -1
+        result.append(dfs(node+i)+1)
+    return min(result)
+
+
+
+print(dfs(0))
