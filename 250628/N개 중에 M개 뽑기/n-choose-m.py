d517b318c36fbcef642a168feea06f74bfdb5af7
Jieun Park <140596167+jieunpark626@users.noreply.github.com>
2025-06-28 00:29:29 +0900

    Title: N개 중에 M개 뽑기, Time: 73ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250628/N개 중에 M개 뽑기/n-choose-m.py
+++ 250628/N개 중에 M개 뽑기/n-choose-m.py
@@ -1,32 +1,21 @@
-# 변수 선언 및 입력
+N, M = map(int, input().split())
 
-n, m = tuple(map(int, input().split()))
-combination = []
+# Please write your code here.
+N, M = map(int, input().split())
 
-
-# 방문한 원소들을 출력해줍니다.
-def print_combination():
-    for elem in combination:
-        print(elem, end = " ")
-    
+def Print(arr):
+    for num in arr:
+        print(num, end=" ")
     print()
 
+nums = []
+def dfs(cur_num, idx):
+    if(cur_num == M+1):
+        Print(nums)
+    for i in range(idx, N+1):
+        nums.append(i)
+        dfs(cur_num+1, i+1)
+        nums.pop()
 
-def find_combination(curr_num, cnt):
-    # n개의 숫자를 모두 탐색했으면 더 이상 탐색하지 않습니다.
-    if curr_num == n + 1:
-        # 탐색하는 과정에서 m개의 숫자를 뽑은 경우 답을 출력해줍니다.
-        if cnt == m:
-            print_combination()
-        return
-
-    # curr_num에 해당하는 숫자를 사용했을 때의 경우를 탐색합니다.
-    combination.append(curr_num)
-    find_combination(curr_num + 1, cnt + 1)
-    combination.pop()
-
-    # curr_num에 해당하는 숫자를 사용하지 않았을 때의 경우를 탐색합니다.
-    find_combination(curr_num + 1, cnt)
-
-
-find_combination(1, 0)
+dfs(1,1)
+        
