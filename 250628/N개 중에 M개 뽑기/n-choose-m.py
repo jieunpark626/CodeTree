f93284884ca34928b2f1d45e117c6c4ffafa1d18
Jieun Park <140596167+jieunpark626@users.noreply.github.com>
2025-06-28 00:24:22 +0900

    Title: N개 중에 M개 뽑기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250628/N개 중에 M개 뽑기/n-choose-m.py
@@ -0,0 +1,18 @@
+N, M = map(int, input().split())
+
+def Print(arr):
+    for num in arr:
+        print(num, end=" ")
+    print()
+
+nums = []
+def dfs(cur_num, idx):
+    if(cur_num == M+1):
+        Print(nums)
+    for i in range(idx, N+1):
+        nums.append(i)
+        dfs(cur_num+1, i+1)
+        nums.pop()
+
+dfs(1,1)
+        
