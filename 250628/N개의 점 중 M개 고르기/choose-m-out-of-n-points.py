7ab167acfdd1d51a297d00546ea02e789e1ab1b8
Jieun Park <140596167+jieunpark626@users.noreply.github.com>
2025-06-28 13:12:46 +0900

    Title: N개의 점 중 M개 고르기, Time: 71ms, Memory: 17MB, Status: Passed - Codetree

--- 250628/N개의 점 중 M개 고르기/choose-m-out-of-n-points.py
+++ 250628/N개의 점 중 M개 고르기/choose-m-out-of-n-points.py
@@ -3,18 +3,18 @@ from itertools import combinations
 n, m = map(int, input().split())
 points = [tuple(map(int, input().split())) for _ in range(n)]
 
-combis = list(combinations(points,m))
+combis = list(combinations(points, m))
 
 dists = []
 for combi in combis:
     max = 0
-    for i in range(0,(m-2)+1):
-        x = (combi[i][0] - combi[i+1][0]) ** 2
-        y = (combi[i][1] - combi[i+1][1]) ** 2
+    items = list(combinations(combi, 2))
+    for item in items:
+        x = (item[0][0] - item[1][0]) ** 2
+        y = (item[0][1] - item[1][1]) ** 2
         result = x+y
-        if(result > max):
+        if (result > max):
             max = result
     dists.append(max)
-    
-    
+
 print(min(dists))
