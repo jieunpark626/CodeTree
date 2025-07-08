929d29497e97012edfc730f104fb8b70ac7631db
Jieun Park <140596167+jieunpark626@users.noreply.github.com>
2025-07-08 21:23:14 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250708/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 250708/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -4,25 +4,21 @@ N = int(input())
 
 q = deque()
 
-
 def bfs():
     while q:
         cur_num_cnt = q.popleft()
-        
+
         if(cur_num_cnt[0] == 1):
             print(cur_num_cnt[1])
-            break
-        for i in range(4):
-            if (i == 0):
-                q.append((cur_num_cnt[0] - 1, cur_num_cnt[1]+1))
-            elif (i == 1):
-                q.append((cur_num_cnt[0] + 1, cur_num_cnt[1]+1))
-            elif (i == 2 and cur_num_cnt[0] % 2 == 0):
-                q.append((cur_num_cnt[0] / 2, cur_num_cnt[1]+1))
-            elif (i == 3 and cur_num_cnt[0] % 3 == 0):
-                q.append((cur_num_cnt[0] / 3, cur_num_cnt[1]+1))
+            return
+        
+        if(cur_num_cnt[0]%3 == 0):
+            q.append((int(cur_num_cnt[0] / 3), cur_num_cnt[1]+1))
+        elif(cur_num_cnt[0]%2==0):
+            q.append((int(cur_num_cnt[0] / 2), cur_num_cnt[1]+1))
+        else:
+            q.append((cur_num_cnt[0] - 1, cur_num_cnt[1]+1))
+            q.append((cur_num_cnt[0] + 1, cur_num_cnt[1]+1))
 
 q.append((N,0))
 bfs()
-        
-        
