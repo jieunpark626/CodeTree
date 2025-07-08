9d770d0484faf8f03e8e4c6001b101bce627ddb9
Jieun Park <140596167+jieunpark626@users.noreply.github.com>
2025-07-09 00:45:33 +0900

    Title: 갈 수 있는 곳들, Time: 56ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250709/갈 수 있는 곳들/places-can-go.py
@@ -0,0 +1,34 @@
+from collections import deque
+n, k = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+points = [tuple(map(int, input().split())) for _ in range(k)]
+
+# Please write your code here.
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+q = deque()
+visited = [[False for _ in range(n)] for _ in range(n)]
+answer = 0
+
+def can_go(x,y):
+    return x>=0 and x<n and y>=0 and y<n and grid[x][y] == 0 and visited[x][y] == False
+
+def bfs():
+    global answer
+    while q:
+        cur_loc = q.popleft()
+        
+        for i in range(4):
+            new_x = cur_loc[0] + dx[i]
+            new_y = cur_loc[1] + dy[i]
+            
+            if(can_go(new_x, new_y)):
+                visited[new_x][new_y] = True
+                q.append((new_x,new_y))
+                answer += 1
+                
+for point in points:
+    q.append((point[0]-1, point[1]-1))
+    bfs()
+print(answer)
