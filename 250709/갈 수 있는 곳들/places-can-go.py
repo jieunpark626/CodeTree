c749490b44ca175187a73a27db52f45f3b999108
Jieun Park <140596167+jieunpark626@users.noreply.github.com>
2025-07-09 00:57:59 +0900

    Title: 갈 수 있는 곳들, Time: 1103ms, Memory: 25MB, Status: Time Limit Exceed - Codetree

--- 250709/갈 수 있는 곳들/places-can-go.py
+++ 250709/갈 수 있는 곳들/places-can-go.py
@@ -9,16 +9,22 @@ dy = [0, 1, 0, -1]
 
 q = deque()
 visited = [[False for _ in range(n)] for _ in range(n)]
-answer = 1
+visited_node = set()
+answer = 0
 
-def can_go(x,y):
+def init_visited():
+    for i in range(n):
+        for j in range(n):
+            visited[i][j] = False
+
+def can_go(x,y): 
     return x>=0 and x<n and y>=0 and y<n and grid[x][y] == 0 and visited[x][y] == False
 
 def bfs():
     global answer
     while q:
         cur_loc = q.popleft()
-        
+
         for i in range(4):
             new_x = cur_loc[0] + dx[i]
             new_y = cur_loc[1] + dy[i]
@@ -26,11 +32,15 @@ def bfs():
             if(can_go(new_x, new_y)):
                 visited[new_x][new_y] = True
                 q.append((new_x,new_y))
-                answer += 1
-                
-for point in points:
-    q.append(point)
-    visited[point[0]][point[1]] = True
-    bfs()
+                visited_node.add((new_x,new_y))
 
-print(answer)
+for point in points:
+    r, c= point[0]-1,point[1]-1
+    
+    q.append((r, c))
+    init_visited()
+    visited[r][c] = True
+    visited_node.add((r,c))
+    bfs()
+    
+print(len(visited_node))
