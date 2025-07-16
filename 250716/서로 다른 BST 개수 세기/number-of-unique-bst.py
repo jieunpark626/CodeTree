d9f067a755619b5e32dfd9ae04d8541ee6a4ecbd
Jieun Park <140596167+jieunpark626@users.noreply.github.com>
2025-07-16 18:17:48 +0900

    Title: 서로 다른 BST 개수 세기, Time: 87ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250716/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+dp = [0 for _ in range(n+1)]
+dp[1] = 1
+dp[2] = 2
+# Please write your code here.
+if(n>=3):
+    for i in range(1,n):
+        dp[n] += dp[i] 
+
+    dp[n] += dp[n-1]
+
+print(dp[n])
+ 
